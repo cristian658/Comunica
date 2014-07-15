08f69e2634513cb3f636823cadae53e794bdaded
Esteban <Esteban@Esteban>
2014-07-15 13:20:12 -0400

    Se elimino comentarios innecesarios...
    Faltarian algunos de los siguientes puntos:
    1- contador en bandeja, ej: "remitente(2)" =>2 mensajes anidados
    2- enviar notificación de (nueva comunicación o respuesta) a email del destinatario
    3- validar si un registro fue leido y no leido (false/true)
    4- validar que la contraseña sea de largo 6

--- src/java/cl/ciisa/comunica/controller/DetalleComunicacionAction.java
+++ src/java/cl/ciisa/comunica/controller/DetalleComunicacionAction.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cl.ciisa.comunica.controller;
 
 import cl.ciisa.comunica.entity.Detallecomunicacion;
@@ -32,7 +27,7 @@ public class DetalleComunicacionAction extends ActionSupport {
 
             if (idComunicacion != null) {
                 this.dcPrimerRegistroDeLaLista = this.detalleComunicaciones.getDetalleComunicacionByIdComunicacionPrincipal(idComunicacion).get(0);            
-
+                
                 if (mensajeDeForm != null) {
                     this.detalleComunicaciones.addRespuesta(mensajeDeForm, null, session.get("correo").toString(), idComunicacion, dcPrimerRegistroDeLaLista);
                     this.setMensajeDeForm("");
